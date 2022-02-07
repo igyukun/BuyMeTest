60af89c84766cf7a3544188040819eecbdb65c6c
igyukun <igorkun@gmail.com>
2022-02-07 20:39:18 +0200

    Adding the last extra assignment: getting the spinner dimensions

--- src/test/java/extras/SpinnerSize.java
+++ src/test/java/extras/SpinnerSize.java
@@ -11,22 +11,42 @@ import utils.InitWebDriverSingleton;
 import java.time.Duration;
 import java.util.function.Function;
 
-public class SpinnerSize {
+/**
+ * Class SpinnerSize locates the spinner icons on the buyme.co.il website loading page
+ * and prints its dimensions to the standard output device.
+ * Note: since the website loading page is shown for the very limited time and on the loaded page the
+ * spinner element cannot be located, the class implements a loop which iterates a limited number of times.
+ * During each iteration the method attempts to locate the spinner element, using fluent wait mechanism.
+ * If the wait timeout is expired the page is refreshed to show the loading page again and to make another attempt
+ * to locate the element.
+ * It is not an ideal solution but it does the job in most cases.
+ * @author Igor Kun
+ * @version 1.0
+ * @since   07-Feb-2022
+ */
 
+public class SpinnerSize {
+    //the locator of the spinner child <div class="bounce2"></div>
     public final By BOUNCE_LOCATOR = By.xpath("/html/body/div[1]/div/div[2]/div/div/div[2]");
 
     WebElement element;
     WebDriver driver;
 
     @BeforeClass
+    /**
+     * Initialize the web driver
+     */
     public void initDriver(){
+
         driver = InitWebDriverSingleton.InitDriver();
         driver.manage().window().maximize();
         driver.get("https://buyme.co.il");
-
     }
 
     @Test
+    /**
+     * The method uses fluent wait API to locate the web element and prints its dimensions when found.
+     */
     public void getSpinnerSize() {
 
         //create fluent wait method
@@ -35,8 +55,8 @@ public class SpinnerSize {
                 pollingEvery(Duration.ofMillis(50)).    //polling timeout
                 ignoring(NoSuchElementException.class); //ignored exception
 
-        //The spinner element is visible only on the loading screen.
-        //iterate up to 10 times while refreshing the webpage each time the wait timeout is expiring
+        //The spinner element is visible only on the loading screen so it is tricky to catch it while the page is being loaded.
+        //The loop iterates up to 10 times, refreshing the webpage each time the wait timeout is expiring
         for (int times = 0, maxIters = 9; times <=maxIters && element == null; times++){
 
             try {
@@ -51,22 +71,30 @@ public class SpinnerSize {
                 break;
 
             }catch (Exception e) {
-                //element could not been found
+                //element could not be found
                 System.out.println(e.getMessage());
                 //refresh the web page
                 driver.navigate().refresh();
-                //throw an exception on the last iteration
+                //throw an exception on the last iteration to let testNG mark the test case as failed.
                 if (times == maxIters) throw e;
             }
         }
     }
 
     @AfterClass
+    /**
+     * Close and quite the web driver and the corresponding browser instance and finalize the test
+     */
     public void tearDown(){
         driver.close();
         driver.quit();
     }
 
+    /**
+     * The function builds an output string by getting the found web element size using two different methods:
+     * getSccValue() and getRect().
+     * @return formatted string with the element dimensions.
+     */
     public String createOutputString(){
         return String.format("%nThe spinner dot size by getCssValue() :%s by %s.%n" +
                         "The spinner dot size by getRect()     :%dpx by %dpx",
